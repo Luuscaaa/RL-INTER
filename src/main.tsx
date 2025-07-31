6082bdc8f9001a32e51a15bd26a8495426d4fdda
Lucas Santana <Lucassantana_m@hotmail.com>
2025-07-30 23:45:01 -0300

    fix: use react-helmet-async for HelmetProvider and update dependencies

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom';
-import { HelmetProvider } from 'react-helmet';
+import { HelmetProvider } from 'react-helmet-async';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>

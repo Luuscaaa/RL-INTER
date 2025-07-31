6cc608d9a1d259829c95322843c0e2a1e573bbe7
Lucas Santana <Lucassantana_m@hotmail.com>
2025-07-30 23:40:34 -0300

    fix: add type declaration for react-helmet

---
+++ src/react-helmet.d.ts
@@ -0,0 +1,5 @@
+declare module 'react-helmet' {
+  import * as React from 'react';
+  export class Helmet extends React.Component<any, any> {}
+  export const HelmetProvider: React.ComponentType<any>;
+}
